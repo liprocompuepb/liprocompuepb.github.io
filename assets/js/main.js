dc3bf5ace8e7d8a87f9c75e5bb0a140d0427a1f5
Liga de Programação Competitiva da UEPB <99844251+liprocompuepb@users.noreply.github.com>
2022-02-20 23:42:01 -0300

    Versão 1.0

---
+++ assets/js/main.js
@@ -0,0 +1,91 @@
+//Menu
+class MobileNavbar {
+  constructor(mobileMenu, navList, navLinks) {
+    this.mobileMenu = document.querySelector(mobileMenu);
+    this.navList = document.querySelector(navList);
+    this.navLinks = document.querySelectorAll(navLinks);
+    this.activeClass = "active";
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  animateLinks() {
+    this.navLinks.forEach((link, index) => {
+      link.style.animation
+        ? (link.style.animation = "")
+        : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+            index / 7 + 0.3
+          }s`);
+    });
+  }
+
+  handleClick() {
+    this.navList.classList.toggle(this.activeClass);
+    this.mobileMenu.classList.toggle(this.activeClass);
+    this.animateLinks();
+  }
+
+  addClickEvent() {
+    this.mobileMenu.addEventListener("click", this.handleClick);
+  }
+
+  init() {
+    if (this.mobileMenu) {
+      this.addClickEvent();
+    }
+    return this;
+  }
+}
+const mobileNavbar = new MobileNavbar(
+  ".mobile-menu",
+  ".nav-list",
+  ".nav-list li"
+);
+mobileNavbar.init();
+
+//Maquina de escrever
+function typeWrite(elemento) {
+  const textoArray = elemento.innerHTML.split("");
+  elemento.innerHTML = " ";
+  textoArray.forEach(function (letra, i) {
+    setTimeout(function () {
+      elemento.innerHTML += letra;
+    }, 100 * i);
+  });
+}
+const titulo = document.querySelector(".maquina-escrever");
+typeWrite(titulo);
+
+//Revelar animação
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "50px",
+  duration: 2000,
+});
+
+//Home delay
+sr.reveal(".button", { delay: 200 });
+sr.reveal(".linguagensProjeto", { delay: 400 });
+sr.reveal(".imagem-espaco-delay", { delay: 200 });
+sr.reveal(".imagem-iuri-delay", { delay: 600 });
+sr.reveal(".scroll-delay", { delay: 1000 });
+//Sobre mim delay
+sr.reveal(".imagem-delay", {});
+sr.reveal(".titulo-delay", { delay: 200 });
+sr.reveal(".descricao-delay", { delay: 400 });
+sr.reveal(".cards-interval", { interval: 400 });
+
+
+document
+  .querySelector(".styled")
+  .addEventListener("mouseover", function mudarTexto8() {
+    texto1.innerHTML =
+      "It is a managed cloud storage service that uses APIs (or VPC endpoints) to store and retrieve data.<br><br> 1 year of experience";
+  });
+
+var sairCaixa = document.getElementsByClassName("sairCaixa");
+
+for (i = 0; i < sairCaixa.length; i++) {
+  sairCaixa[i].addEventListener("mouseout", function mudarTextoNormal() {
+    texto1.innerHTML = `< Passe o mouse no card para ler >`;
+  });
+}
